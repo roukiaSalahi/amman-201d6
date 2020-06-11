16fee5b7a8b41955c9077129c6d01e60477cc059
roukiaSalahi <roqaia.sa@hotmail.com>
2020-06-11 13:57:14 +0300

    question3 is solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+var total=a+b+c;
+var product=a*b*c;
+var message1 = '4 and 7 and 5 sum to 16.'
+var message2 = 'The product of 4 and 7 and 5 is 140.'
+return [total,product,message1,message2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
